fef198f96b71247d9eca40967f29b8388a5412dc
Administrator <Administrator@DELL>
2021-03-16 19:06:45 +0530

    Testing 2

---
+++ WINIUMDESKTOPAPPTEST/src/test/java/desktopappTest/Addition2.java
@@ -0,0 +1,34 @@
+package desktopappTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.winium.DesktopOptions;
+import org.openqa.selenium.winium.WiniumDriver;
+import org.testng.annotations.Test;
+
+public class Addition2 {
+	
+
+	@Test
+	public void addition() throws MalformedURLException {
+		
+		DesktopOptions option=new DesktopOptions();
+		
+		option.setApplicationPath("C:\\Windows\\System32\\calc.exe");
+		
+		WiniumDriver driver=new WiniumDriver(new URL("http://localhost:9999"), option);
+		
+		driver.findElement(By.name("Seven")).click();
+		
+		driver.findElement(By.name("Plus")).click();
+
+		driver.findElement(By.name("Nine")).click();
+		
+		driver.findElement(By.name("Equals")).click();
+
+		System.out.println("Test Success");
+	}
+
+}
